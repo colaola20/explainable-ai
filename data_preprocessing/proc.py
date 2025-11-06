c0fd2190798fe1a9f603e81747589a2d2bedc8e2
colaola20 <soryo@farmingdale.edu>
2025-11-06 13:30:09 -0500

    Restructured files. Added jupyter notebook for data visualization and exploration.

--- data_preprocessing/proc.py
+++ data_preprocessing/proc.py
@@ -6,7 +6,7 @@ import seaborn as seaborn
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 100)
 
-df = pd.read_csv("dataset/raw.csv", header=1)
+df = pd.read_csv("data_preprocessing/data/raw.csv", header=1)
 df.columns = df.columns.str.strip().str.lower().str.replace(r'\s+', '_', regex=True)
 df.rename(columns={'default_payment_next_month': 'default'}, inplace=True)
 
