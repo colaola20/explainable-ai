fe366d7f34bae179ef0fbaaf03abe6a9489353d7
Esteban Sandoval <sandef3@farmingdale.edu>
2025-11-06 10:27:41 -0500

    Incomplete* (transfer to home computer)raw data added and started processing class.

---
+++ dataset/proc.py
@@ -0,0 +1,8 @@
+import pandas as pandas
+import numpy as numpy
+import matplotlib.pyplot as matplot
+import seaborn as seaborn
+
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_rows', 100)
+
