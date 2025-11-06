83de8aebc39388e95b81a4a836d8e18b074a60f9
colaola20 <soryo@farmingdale.edu>
2025-11-06 12:55:42 -0500

    Working with understanding my data

--- dataset/proc.py
+++ dataset/proc.py
@@ -1,8 +1,18 @@
-import pandas as pandas
+import pandas as pd
 import numpy as numpy
 import matplotlib.pyplot as matplot
 import seaborn as seaborn
 
-pandas.set_option('display.max_columns', None)
-pandas.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 100)
+
+df = pd.read_csv("dataset/raw.csv", header=1)
+df.columns = df.columns.str.strip().str.lower().str.replace(r'\s+', '_', regex=True)
+df.rename(columns={'default_payment_next_month': 'default'}, inplace=True)
+
+print(df.columns)
+print(df.shape)
+print(df.head())
+print(df.info())
+
 
